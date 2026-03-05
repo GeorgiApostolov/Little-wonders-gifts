9e940ad29782f01a0615f9a891ac9cc73163925f
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2026-03-06 00:10:43 +0200

    update

--- WEBSITE/.next/server/middleware-build-manifest.js
+++ WEBSITE/.next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-3134317e017aa746.js","static/chunks/4bd1b696-c023c6e3521b1417.js","static/chunks/255-9078e8c409b3da86.js","static/chunks/main-app-ab0ddc60decc32a5.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-3134317e017aa746.js","static/chunks/framework-a6e0b7e30f98059a.js","static/chunks/main-1caf917d3a12801a.js","static/chunks/pages/_app-c0ee45b59225b19e.js"],"/_error":["static/chunks/webpack-3134317e017aa746.js","static/chunks/framework-a6e0b7e30f98059a.js","static/chunks/main-1caf917d3a12801a.js","static/chunks/pages/_error-76cc6b105d9ffa16.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- WEBSITE/.next/types/app/kontakti/page.ts
+++ WEBSITE/.next/types/app/kontakti/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\HP\Documents\GitHub\Little-wonders-gifts\WEBSITE\src\app\kontakti\page.tsx
+// File: /Users/georgiapostolov/Desktop/github/Little-wonders-gifts/WEBSITE/src/app/kontakti/page.tsx
 import * as entry from '../../../../src/app/kontakti/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
